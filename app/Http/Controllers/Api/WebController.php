729484371033ebc8122fdc3c2315d47f6d02fe6d
AHF 03 <ahf03@AHFs-Air.localdomain>
2024-10-21 16:47:03 +0300

    first commit

---
+++ app/Http/Controllers/Api/WebController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    //
+
+    public function index(){
+
+        
+    }
+
+}
